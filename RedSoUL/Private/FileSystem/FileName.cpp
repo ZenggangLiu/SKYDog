ddd8714ff11624c9d869e6ad22e29ff7f108cbd7
Zenggang LIU <zenggang.liu.1973@gmail.com>
2026-08-16 21:32:15 +1200

    +ADDED: 添加了base_name 获取文件Base name的函数

--- RedSoUL/Private/FileSystem/FileName.cpp
+++ RedSoUL/Private/FileSystem/FileName.cpp
@@ -7,6 +7,52 @@
 #include "FileSystem/FileName.hpp"
 
 
+int32_t
+FileName::base_name (
+    const char * const file_name,
+    char * const       buffer_start,
+    const uint32_t     buffer_size)
+{
+    RUNTIME_ASSERT( file_name,    "File name can not be NULL!!");
+    RUNTIME_ASSERT(*file_name,    "File name can not be empty!!");
+    RUNTIME_ASSERT( buffer_start, "Output buffer can not be NULL!!");
+    RUNTIME_ASSERT( buffer_size,  "Output buffer size can not be 0!!");
+
+    int32_t base_name_length = (int32_t)std::strlen(file_name);
+
+    /// 从文件名尾部开始向前查找'.'分隔符
+    for (int32_t c = base_name_length - 1; c >= 0; --c)
+    {
+        /// 如果找到目录分隔符: 停止查找
+        if (file_name[c] == '/' || file_name[c] == '\\')
+        {
+            break;
+        }
+        /// 如果找到'.'分隔符
+        else if (file_name[c] == '.')
+        {
+            base_name_length = c;
+            break;
+        }
+    }
+    RUNTIME_ASSERT(buffer_size >= (uint32_t)base_name_length + 1, /// 字符串结束符'\0'占用一个字节
+                   "Output buffer is too small!!");
+
+    if (buffer_size >= (uint32_t)base_name_length + 1) /// 字符串结束符'\0'占用一个字节
+    {
+        /// 复制'.'前方的字符串
+        std::memcpy(buffer_start, file_name, base_name_length);
+        /// 保存结束符'\0'
+        buffer_start[base_name_length] = 0;
+        return base_name_length;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+
 bool
 FileName::append_file_extension (
     const char * const file_name,
@@ -22,34 +68,14 @@ FileName::append_file_extension (
     RUNTIME_ASSERT( buffer_size,  "Output buffer size can not be 0!!");
 
     const uint32_t extension_length = (uint32_t)std::strlen(extension);
-
-    /// 保存'.'的位置(如果指定文件名无扩展名, '.'的位置将设定在字符串尾)
-    uint32_t dot_pos = (uint32_t)std::strlen(file_name);
-
-    /// 从文件名尾部开始向前查找'.'分隔符
-    for (int32_t c = (int32_t)(dot_pos - 1); c >= 0; --c)
+    const int32_t  base_name_length = base_name(file_name, buffer_start, buffer_size);
+    if (base_name_length > 0 &&  /// '.' 与 '\0'字符各占一个空间
+        buffer_size >= (uint32_t)base_name_length + extension_length + 2) /// '.', '\0'
     {
-        /// 如果找到目录分隔符: 停止查找
-        if (file_name[c] == '/' || file_name[c] == '\\')
-        {
-            break;
-        }
-        /// 如果找到'.'分隔符
-        else if (file_name[c] == '.')
-        {
-            dot_pos = (uint32_t)c;
-            break;
-        }
-    }
-    RUNTIME_ASSERT(buffer_size >= dot_pos + extension_length + 1, /// '.'分隔符占一个字符
-                   "Output buffer is too small!!");
-
-    if (buffer_size >= dot_pos + extension_length + 1)
-    {
-        /// 复制'.'前方的字符串
-        std::memcpy(buffer_start, file_name, dot_pos);
         /// 添加扩展名
-        std::snprintf(buffer_start + dot_pos, buffer_size - dot_pos, ".%s", extension);
+        std::snprintf(
+            buffer_start + base_name_length,
+            buffer_size  - base_name_length, ".%s", extension);
         return true;
     }
     else
--- RedSoUL/Public/FileSystem/FileName.hpp
+++ RedSoUL/Public/FileSystem/FileName.hpp
@@ -36,6 +36,25 @@
 ///
 struct FileName
 {
+    /// 获取文件的base name
+    /// FileName为: BaseName.Extension
+    ///
+    /// @param[in]  file_name
+    ///     文件名
+    /// @param[out] buffer_start
+    ///     用来保存base name的buffer
+    /// @param[in]  buffer_size
+    ///     上面Buffer的字节大小
+    /// @return
+    ///     base name的字符长度, 如果成功获取
+    ///     -1: 如果获取失败
+    static
+    int32_t
+    base_name (
+        const char * const file_name,
+        char * const       buffer_start,
+        const uint32_t     buffer_size);
+
     /// 添加指定扩展名
     /// NOTE: 如果指定的文件名已带有扩展名, 其将被替代
     ///
@@ -43,6 +62,10 @@ struct FileName
     ///     文件名
     /// @param[in]  extension
     ///     扩展名. 例如: "ppm"
+    /// @param[out] buffer_start
+    ///     用来保存添加扩展名后的文件名的buffer
+    /// @param[in]  buffer_size
+    ///     上面Buffer的字节大小
     /// @return
     ///     True    如果添加成功
     ///     False   如果添加失败
