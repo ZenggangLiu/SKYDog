4887eb3bb54358647d0eaa27a9720b95539386fb
Zenggang LIU <zenggang.liu.1973@gmail.com>
2026-08-23 23:23:41 +1200

    +ADDED：添加了RenderTexture类型

---
+++ RedSoUL/Private/Render/RenderTexture.cpp
@@ -0,0 +1,45 @@
+/// Library headers
+#include "Assert/RuntimeAssert.hpp"
+#include "Render/RenderTextureDepot.hpp"
+/// Self header
+#include "Render/RenderTexture.hpp"
+
+
+RenderTexture::RenderTexture (
+    const RenderTextureIdT _texture_id,
+    const TextureDataType  _texture_data_type,
+    const uint32_t         _texture_width,
+    const uint32_t         _texture_height,
+    const uint32_t         _slice_data_size,
+    const uint8_t          _mipmap_count,
+    const bool             _is_linear_rgb,
+    const bool             _is_cube_map,
+    const bool             _is_dyn_allocated,
+    const uint8_t * const  _texel_list,
+    const uint32_t         _texel_list_size)
+:
+    texel_list(_texel_list),
+    texture_id(_texture_id),
+    texel_list_size(_texel_list_size),
+    texture_width(_texture_width),
+    texture_height(_texture_height),
+    slice_data_size(_slice_data_size),
+    mipmap_count(_mipmap_count),
+    texture_data_type(_texture_data_type),
+    is_linear_rgb(_is_linear_rgb),
+    is_cube_map(_is_cube_map),
+    is_dyn_allocated(_is_dyn_allocated)
+{
+    RUNTIME_ASSERT(_texture_id != INVALID_RENDER_TEXTURE_ID, "Texture Id is invalid!!");
+    RUNTIME_ASSERT(_texture_width  > 0, "Texture width must be larger than zero!!");
+    RUNTIME_ASSERT(_texture_height > 0, "Texture height must be larger than zero!!");
+    RUNTIME_ASSERT(_is_cube_map == false || _texture_width == _texture_height,
+                   "Cube map texture width and height must be the same!!");
+    RUNTIME_ASSERT(_slice_data_size, "Zero slice data size!!");
+    RUNTIME_ASSERT(_texel_list, "No texel list!!");
+    RUNTIME_ASSERT(_texel_list_size, "Zero texel list size!!");
+    RUNTIME_ASSERT(
+        _texel_list_size ==
+        _slice_data_size * RenderTextureDepot::ref().total_slice_count(_is_cube_map),
+        "Texel list size does not match slice data size!!");
+}
--- RedSoUL/Private/Render/RenderTexture.hpp
+++ RedSoUL/Private/Render/RenderTexture.hpp
@@ -17,8 +17,8 @@
     Author:   (___()'`; Zee...  \_/|_)     )                                            
               /,    /`             \  __  /                                             
               \\"--\\              (_/ (_/                                              
-    Created:  9/08/26  @  10:31 PM
-    FileName: TextureDataType.hpp @ RedSoUL Project
+    Created:  9/08/26  @  10:38 PM
+    FileName: RenderTexture.hpp @ RedSoUL Project
     History:
              - created by: 9/08/26: Zenggang LIU
                                                                                         
@@ -29,18 +29,64 @@
 
 
 /// System headers
-#include <stdint.h> /// uint8_t
+#include <stdint.h> /// uint32_t,...
+/// Library headers
+#include "Render/RenderTextureId.hpp"
+#include "Render/TextureDataType.hpp"
 
 
-/// Render texture(渲染贴图)数据类型
+/// Texel数据Layout: Slice0, Slice1, ..., SliceN
+/// NOTE: 贴图由N个连续的Slice构成
 ///
-enum class TextureDataType : uint8_t
+///                  +-----------------------+
+///                  |        Slice N        |
+///          ~~~     |         Mip 0         |
+/// +-----------------------+                |
+/// |        Slice 0        |----+-----------+
+/// |         Mip 0         | 1  | <- Mip 1
+/// |                       |    |
+/// +-----------+-----------+----+
+/// |   Mip 1   |    | Mip 2 |
+/// |           |    +-------+
+/// +-------+---+
+/// | Mip 2 |
+/// +-------+
+///
+struct RenderTexture
 {
-    // === 非压缩数据类型 === //
-    /// Ldr Rgba 四通道彩色贴图: 数据保存为 LdrColor[]
-    LDR_RGBA_TEXTURE,
-    /// Ldr Grayscale单通道灰度贴图: 数据保存为 LdrGrayscale[]
-    LDR_GRAYSCALE_TEXTURE,
-    /// Hdr Rgba 四通道彩色贴图: 数据保存为 HdrColor[]
-    HALF_RGBA_TEXTURE,
+    /// Texel数据列表
+    const uint8_t * const  texel_list;
+    /// Texture Id
+    const RenderTextureIdT texture_id;
+    /// Texel数据列表大小(字节数)
+    const uint32_t         texel_list_size;
+    /// 贴图宽度(Mipmap0横向Texel数)
+    const uint32_t         texture_width;
+    /// 贴图高度(Mipmap0纵向Texel数)
+    const uint32_t         texture_height;
+    /// Slice中的所有Texel数据大小(字节数|即, 所有Mipmap中Texel的总和)
+    const uint32_t         slice_data_size;
+    /// Mipmap总数。如果, 无MipMap, 则设定为1
+    const uint8_t          mipmap_count;
+    /// 贴图数据类型
+    const TextureDataType  texture_data_type;
+    /// 标记Texel数据是否使用Linear Rgb空间(True), 还是使用Gamma encoded Rgb空间(False)
+    const bool             is_linear_rgb;
+    /// 标记贴图是否为CubeMap(True)
+    const bool             is_cube_map;
+    /// 标记Texel数据是否否为动态分配(True)
+    const bool             is_dyn_allocated;
+
+    RenderTexture (
+        const RenderTextureIdT texture_id,
+        const TextureDataType  texture_data_type,
+        const uint32_t         texture_width,
+        const uint32_t         texture_height,
+        const uint32_t         slice_data_size,
+        const uint8_t          mipmap_count,
+        const bool             is_linear_rgb,
+        const bool             is_cube_map,
+        const bool             is_dyn_allocated,
+        const uint8_t * const  texel_list,
+        const uint32_t         texel_list_size);
 };
